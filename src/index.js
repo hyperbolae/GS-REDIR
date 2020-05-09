6c9434b746f2a208784870c898b33eb4482464b6
hyperbola <hyperbola.cs07@gmail.com>
2020-05-09 13:04:55 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+require('dotenv').config();
 const http = require("http");
 const url = require("url");
 
-http.createServer(function (req, res) {
+const port = process.env.PORT || 8080;
+const server = http.createServer(function (req, res) {
 
     let param = url.parse(req.url, true).query;
     const s = param.s;
     
     let redir;
-    if (!s) redir = "https://www.google.com/";
-    else if (s.match(/^[\x00-\x7F]*$/)) redir = `https://www.google.com/search?q=${encodeURIComponent(s)}&lr=lang_en`;
-    else redir = `https://www.google.com/search?q=${encodeURIComponent(s)}&lr=lang_zh-TW`;
+    if (!s) { 
+		redir = "https://www.google.com/";
+	}
+	else {
+		const lang = s.match(/^[\x00-\x7F]*$/) ? "lang_en" : "lang_zh-TW";
+		redir = `https://www.google.com/search?q=${encodeURIComponent(s)}&lr=${lang}`;
+	}
     
     res.writeHead(302, { "Location": redir });
     res.end();
+});
 
-}).listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080, () => {
+	console.log(`Server running on port ${port}`);
+});
+
